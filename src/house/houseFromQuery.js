a8dcaea1a6c14d9c3af1423957366fcc58cdc188
pauljmartin16 <pauljmartin16@gmail.com>
2022-03-30 15:52:15 -0400

    CR-0 lazy loading of selected house from react-router-dom

--- src/house/houseFromQuery.js
+++ src/house/houseFromQuery.js
@@ -2,11 +2,23 @@ import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import House from './';
 import HouseContext from '../context/houseContext';
+import useHouses from '../hooks/useHouses';
+
 
 const HouseFromQuery = () => {
     const { id } = useParams();
 
-    const allHouses = useContext(HouseContext);
+    // pjm - it's possible Lazy doesnt inherit the data context. Needs further investigation
+    // this is a quick hack to load an address for lazy loading
+    const allHouses = [{
+        "id": id,
+        "address": "12 Valley of Kings, Geneva",
+        "country": "Switzerland",
+        "description": "A superb detached Victorian property on one of the town's finest roads, within easy reach of Barnes Village. The property has in excess of 6000 sq/ft of accommodation, a driveway and landscaped garden.",
+        "price": 900000,
+        "photo": "277667"
+    }];
+
 
     const house = allHouses.find((h) => h.id == parseInt(id));
 
--- src/main-page/index.js
+++ src/main-page/index.js
@@ -4,10 +4,12 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import FeaturedHouse from './featured-house';
 import SearchResults from '../search-results';
 import HouseFilter from './house-filter';
-import HouseFromQuery from '../house/houseFromQuery';
 import useHouses from '../hooks/useHouses';
 import useFeaturedHouse from '../hooks/useFeaturedHouse';
+import React, { Suspense } from 'react';
 import HouseContext from '../context/houseContext';
+// import HouseFromQuery from '../house/houseFromQuery';
+const LazyHouseFromQuery = React.lazy(() => import ('../house/houseFromQuery'));
 
 function App() {
   // importing custom hook
@@ -18,13 +20,18 @@ function App() {
   return (
     <Router>
       <HouseContext.Provider value={allHouses}>
-
         <div className='container'>
           <Header subtitle="Providing houses all over the world" />
           <HouseFilter allHouses={allHouses} />
           <Routes>
             <Route path="/searchResults/:country" element={<SearchResults  />} />
-            <Route path="/house/:id" element={<HouseFromQuery />} />
+            {/* <Route path="/house/:id" element={<HouseFromQuery />} /> */}
+            <Route path="/house/:id" element=
+            {
+              <Suspense fallback={<div>Loading Lazy House from Query fallback msg</div>}>
+                <LazyHouseFromQuery />
+              </Suspense>
+            } />
             <Route path="/" element = {<FeaturedHouse house={featuredHouse} />} />
           </Routes>
         </div>
